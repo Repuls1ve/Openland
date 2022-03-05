7de3d2e2a562057483635d4f39506ff2041a359b
Repuls1ve <repulsive@list.ru>
2022-03-05 14:40:15 +0300

    onboarding layout

--- client/src/app/features/onboarding/onboarding.module.ts
+++ client/src/app/features/onboarding/onboarding.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core'
 import { SharedModule } from 'src/app/shared/shared.module'
+import { LayoutComponent } from './containers/layout/layout.component'
 import { OnboardingRoutingModule } from './routing/onboarding-routing.module'
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LayoutComponent
+  ],
   imports: [
     SharedModule,
     OnboardingRoutingModule
